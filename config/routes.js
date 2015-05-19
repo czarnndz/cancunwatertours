502ab78c0dbd663e349d37bb1293ee647fd36131
Carlos Barahona <ingbarahonacarlos@hotmail.com>
2015-05-19 17:59:12 -0500

    config

--- config/routes.js
+++ config/routes.js
@@ -41,6 +41,9 @@ module.exports.routes = {
   '/resultados': {
     view: 'home/resultados'
   },
+  '/detalle': {
+    view: 'home/detalle'
+  },
 
   /***************************************************************************
   *                                                                          *
