f8b3f6938fafe1bb2058fa6d2c0b2e59bc787830
Carlos Barahona <ingbarahonacarlos@hotmail.com>
2015-05-20 14:04:50 -0500

    detalle

---
+++ assets/js/funciones.js
@@ -0,0 +1,15 @@
+/*detalles*/
+$(document).ready(function() {
+
+  var owl = $(".cont-imagen-detalle");
+
+  owl.owlCarousel({
+      items : 1, //10 items above 1000px browser width
+      itemsDesktop : [1000,1], //5 items between 1000px and 901px
+      itemsDesktopSmall : [900,1], // betweem 900px and 601px
+      itemsTablet: [600,1], //2 items between 600 and 0
+      itemsMobile : [400,1] // itemsMobile disabled - inherit from itemsTablet option
+    });
+
+});
+/*detalles*/
