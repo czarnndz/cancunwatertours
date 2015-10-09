78a237099ae466a71906b96bd95bc4c90077b55a
jafet gonzalez <detoxikaided@gmail.com>
2015-10-09 16:12:05 -0500

    bootstrap tests

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,30 +8,12 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
  */
-var killable = require('killable'),
-    server,
-    http = require('http'),
-    port = process.env.PORT || 1337;
 
 module.exports.bootstrap = function (cb) {
 
     // It's very important to trigger this callack method when you are finished
     // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
 
-    //cb();
-    server.kill(function(err){
-        cb();
-    });
+    cb();
 };
 
-server = http.createServer(function(req, res){
-    res.end('loading Cancun water tours... ');
-}).listen(port, function(){
-});
-
-killable(server);
-module.exports.bootstrap = function(cb) {
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
-};
