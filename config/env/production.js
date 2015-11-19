42ee38a0e355f2e15553a26c30287beebb194b5a
Luis <luis19prz@gmail.com>
2015-11-19 16:33:02 -0500

    env fix

--- config/env/production.js
+++ config/env/production.js
@@ -32,7 +32,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: process.env.PORT || 1195
+  port: process.env.PORT || 1195,
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
