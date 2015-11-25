c2783ab720df8333dfba2510d26ef7236848a71d
Luis <luis19prz@gmail.com>
2015-11-25 17:54:45 -0500

    exp lazyload

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,10 +10,16 @@ var app = angular.module('watertours',[
 app.config(['localStorageServiceProvider','lazyImgConfigProvider', function(localStorageServiceProvider, lazyImgConfigProvider){
   localStorageServiceProvider.setPrefix('ls');
 
+  var wrapper = document.querySelector('#home-photos');
   lazyImgConfigProvider.setOptions({
     offset: 100, // how early you want to load image (default = 100)
     errorClass: 'error', // in case of loading image failure what class should be added (default = null)
     successClass: 'lazy-load-complete', // in case of loading image success what class should be added (default = null)
+    onSuccess: function(image){
+      //console.log('success');
+      //console.log(image);
+    },
+    container: angular.element(wrapper)
   });
 
 }]);
